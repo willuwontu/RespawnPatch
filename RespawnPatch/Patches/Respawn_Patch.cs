359b3abe25e28036393b6f316fbd148bd00e002f
willuwontu <willuwontu3@gmail.com>
2022-01-06 11:24:02 -0800

    Initial commit

---
+++ RespawnPatch/Patches/Respawn_Patch.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace RespawnPatch.Patches
+{
+    [HarmonyPatch(typeof(HealthHandler), "RPCA_Die_Phoenix")]
+    class Respawn_Patch
+    {
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            var codes = new List<CodeInstruction>(instructions);
+
+            for (var i = 0; i < codes.Count; i++)
+            {
+                UnityEngine.Debug.Log($"{i}: {codes[i].opcode}, {codes[i].operand}");
+            }
+
+            return codes.AsEnumerable();
+        }
+    }
+}
---
+++ RespawnPatch/RespawnPatch.cs
@@ -0,0 +1,47 @@
+﻿using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+using System;
+using System.Linq;
+using Photon.Pun;
+
+namespace RespawnPatch
+{
+    // Declares our mod to Bepin
+    [BepInPlugin(ModId, ModName, Version)]
+    // The game our mod is associated with
+    [BepInProcess("Rounds.exe")]
+    public class RespawnPatch : BaseUnityPlugin
+    {
+        private const string ModId = "com.willuwontu.rounds.RespawnPatch";
+        private const string ModName = "Respawn Patch";
+        public const string Version = "1.0.0"; // What version are we on (major.minor.patch)?
+
+
+
+        void Awake()
+        {
+            // Use this to call any harmony patch files your mod may have
+            var harmony = new Harmony(ModId);
+            harmony.PatchAll();
+        }
+
+        internal static void CallReduceRespawns(int playerID)
+        {
+            Player player = PlayerManager.instance.players.Where(p => p.playerID == playerID).First();
+
+            if (PhotonNetwork.IsMasterClient)
+            {
+                player.data.view.RPC(nameof(RPCA_ReduceRespawns), RpcTarget.All, playerID);
+            }
+        }
+
+        [PunRPC]
+        private void RPCA_ReduceRespawns(int playerID)
+        {
+            var player = PlayerManager.instance.players.Where(p => p.playerID == playerID).First();
+
+            player.data.stats.remainingRespawns--;
+        }
+    }
+}
